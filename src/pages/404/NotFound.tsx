23f6de75530fe6aed14d4dc6956343789461f831
Geeth Wishkamal <41125548+geethwish@users.noreply.github.com>
2024-12-03 22:23:40 +0530

    footer and page not found added

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <div className='page-center'>
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+        </div>
     )
 }
 
